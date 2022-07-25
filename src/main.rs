dc7e85d26b5f76eefaa634c33cfec7ec87926565
Vishal Patil <vishpat@gmail.com>
2022-07-24 22:33:10 -0400

    Checkpoint

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate jsonwebtoken as jwt;
 use actix_web::{get, post, web, App, HttpRequest, HttpServer, Responder};
 use jwk::JsonWebKey;
 use serde::{Deserialize, Serialize};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct UserInfo {
@@ -15,22 +16,23 @@ struct KeyData {
 
 #[derive(serde::Serialize, serde::Deserialize)]
 struct TokenClaims {
-    exp: usize,
+    exp: u64,
+    iat: u64,
     sub: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct JWKS {
+struct Jwks {
     keys: Vec<JsonWebKey>,
 }
 
 #[get("/jwks")]
-async fn jwks(req: HttpRequest) -> web::Json<JWKS> {
+async fn jwks(req: HttpRequest) -> web::Json<Jwks> {
     let local_data = req
         .app_data::<web::Data<KeyData>>()
         .expect("Key Data not found");
     let jwks = vec![local_data.key.clone()];
-    let keys = JWKS { keys: jwks };
+    let keys = Jwks { keys: jwks };
     web::Json(keys)
 }
 
@@ -38,25 +40,31 @@ async fn jwks(req: HttpRequest) -> web::Json<JWKS> {
 async fn token(user_info: web::Json<UserInfo>, state: web::Data<KeyData>) -> impl Responder {
     let jwk = state.key.clone();
     let alg: jwt::Algorithm = jwk.algorithm.unwrap().into();
+    let ctime = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
 
-    let token = jwt::encode(
-            &jwt::Header::new(alg),
-            &TokenClaims {
-                exp: 0,
-                sub: user_info.name.clone(),
-            },
-            &jwk.key.to_encoding_key(),
-        ).unwrap();
-           
-    token
+    jwt::encode(
+        &jwt::Header::new(alg),
+        &TokenClaims {
+            exp: ctime + 3600,
+            iat: ctime,
+            sub: user_info.name.clone(),
+        },
+        &jwk.key.to_encoding_key(),
+    )
+    .unwrap()
 }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let key = jwk::Key::generate_p256();
     let mut my_jwk = jwk::JsonWebKey::new(key);
-    my_jwk.set_algorithm(jwk::Algorithm::ES256).expect("Failed to set algorithm");
-    let data = web::Data::new(KeyData { key: my_jwk});
+    my_jwk
+        .set_algorithm(jwk::Algorithm::ES256)
+        .expect("Failed to set algorithm");
+    let data = web::Data::new(KeyData { key: my_jwk });
 
     HttpServer::new(move || {
         App::new().service(
