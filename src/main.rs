ed1a391f2939f0f0d41c446c9025a5e83344c8bf
Vishal Patil <vishpat@gmail.com>
2022-07-24 22:05:59 -0400

    Able to get JWT token

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate jsonwebkey as jwk;
+extern crate jsonwebtoken as jwt;
 use actix_web::{get, post, web, App, HttpRequest, HttpServer, Responder};
 use jwk::JsonWebKey;
 use serde::{Deserialize, Serialize};
@@ -12,13 +13,19 @@ struct KeyData {
     key: JsonWebKey,
 }
 
+#[derive(serde::Serialize, serde::Deserialize)]
+struct TokenClaims {
+    exp: usize,
+    sub: String,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct JWKS {
     keys: Vec<JsonWebKey>,
 }
 
 #[get("/jwks")]
-async fn jwks(req: HttpRequest) -> impl Responder {
+async fn jwks(req: HttpRequest) -> web::Json<JWKS> {
     let local_data = req
         .app_data::<web::Data<KeyData>>()
         .expect("Key Data not found");
@@ -28,16 +35,28 @@ async fn jwks(req: HttpRequest) -> impl Responder {
 }
 
 #[post("/token")]
-async fn token(user_info: web::Json<UserInfo>) -> impl Responder {
-    format!("{}", user_info.name);
-    format!("Get token for user: {}", user_info.name)
+async fn token(user_info: web::Json<UserInfo>, state: web::Data<KeyData>) -> impl Responder {
+    let jwk = state.key.clone();
+    let alg: jwt::Algorithm = jwk.algorithm.unwrap().into();
+
+    let token = jwt::encode(
+            &jwt::Header::new(alg),
+            &TokenClaims {
+                exp: 0,
+                sub: user_info.name.clone(),
+            },
+            &jwk.key.to_encoding_key(),
+        ).unwrap();
+           
+    token
 }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let key = jwk::Key::generate_p256();
-    let my_jwk = jwk::JsonWebKey::new(key);
-    let data = web::Data::new(KeyData { key: my_jwk });
+    let mut my_jwk = jwk::JsonWebKey::new(key);
+    my_jwk.set_algorithm(jwk::Algorithm::ES256).expect("Failed to set algorithm");
+    let data = web::Data::new(KeyData { key: my_jwk});
 
     HttpServer::new(move || {
         App::new().service(
